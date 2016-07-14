fd3a9046b05df44146bec8d46f5aeb02b953aabb
satya <akash581050@gmail.com>
2016-07-14 10:54:54 +0530

    THE FILE HAS BEEN SUCCESSFULLY COMMITTED

---
+++ README.md
@@ -0,0 +1,1 @@
+# File_Reader By Prince Satya By Using The Module Pyttsx So Friends Before You Run You Have To Install This Module
---
+++ text_to_speech.py
@@ -0,0 +1,23 @@
+from pyttsx import *
+from Tkinter import *
+from time import sleep
+from tkFileDialog import *
+import tkMessageBox as msg
+root=Tk()
+en=init()
+a=raw_input("DO YOU WANT TO READ A FILE\nAnswer Y/N\n$$")
+if a=='Y':
+    open_file=askopenfile(parent=root,mode='rb',title="File To Be Read")
+if open_file !=None:
+    content=open_file.read()
+    i=content
+    en.say(i)
+if a=='N':
+    i=raw_input("Enter Message To Be Told")
+    en.say(i)
+
+rate=en.getProperty('rate')
+en.setProperty('rate',rate-50)
+en.runAndWait()
+msg.showinfo("hey",message="This Is The End Of The File Run Again To Read Another")
+root.destroy()
